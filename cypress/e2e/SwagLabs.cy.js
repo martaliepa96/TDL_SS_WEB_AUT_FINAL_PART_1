49e887a9fae6e9314dc7851302eca4d630151295
Student17 <student17@school-7715.local>
2022-08-25 15:20:44 +0300

    swag labs scenarios

--- cypress/e2e/SwagLabs.cy.js
+++ cypress/e2e/SwagLabs.cy.js
@@ -1,5 +1,7 @@
 import HomePage from "../pageObjects/Home.page";
+// import ItemPage from "../pageObjects/ItemPage";
 import LoginPage from "../pageObjects/Login.page";
+// import SortItems from "../pageObjects/SortItems.page";
 
 describe("Swag Labs", () => {
   beforeEach(() => {
@@ -8,5 +10,137 @@ describe("Swag Labs", () => {
 
   it("Scenario 1", () => {
     // TODO: implement me
+    LoginPage.UsernameField.click();
+    LoginPage.UsernameField.type('locked_out_user');
+    LoginPage.PasswordField.click();
+    LoginPage.PasswordField.type('secret_sauce');
+    LoginPage.LoginButton.click();
+    LoginPage.Notification.should('contain.text', 'Epic sadface: Sorry, this user has been locked out.');
   });
+  it("Scenario 2", () => {
+    LoginPage.visit();
+    LoginPage.UsernameField.click();
+    LoginPage.UsernameField.type('standard_user');
+    LoginPage.PasswordField.click();
+    LoginPage.PasswordField.type('parole');
+    LoginPage.LoginButton.click();
+    LoginPage.Notification.should('contain.text', 'Epic sadface: Username and password do not match any user in this service');
+  });
+  it("Scenario 3", () => {
+    LoginPage.visit();
+    LoginPage.UsernameField.click();
+    LoginPage.UsernameField.type('standard_user');
+    LoginPage.PasswordField.click();
+    LoginPage.PasswordField.type('secret_sauce');
+    LoginPage.LoginButton.click();
+    LoginPage.Default.should('have.length', '6');
+    
+  })
+  it("Scenario 4", () => {
+    LoginPage.visit();
+    LoginPage.UsernameField.click();
+    LoginPage.UsernameField.type('standard_user');
+    LoginPage.PasswordField.click();
+    LoginPage.PasswordField.type('secret_sauce');
+    LoginPage.LoginButton.click();
+    LoginPage.SortButton.select("Price (high to low)");
+    LoginPage.FirstItem.should("have.text", "Sauce Labs Fleece Jacket");
+    LoginPage.ItemCosts.should("have.text", "$49.99"); 
+  })
+
+  it("Scenario 5", () => {
+    LoginPage.visit();
+    LoginPage.UsernameField.click();
+    LoginPage.UsernameField.type('standard_user');
+    LoginPage.PasswordField.click();
+    LoginPage.PasswordField.type('secret_sauce');
+    LoginPage.LoginButton.click();
+    LoginPage.SortButton.select("Price (low to high)");
+    LoginPage.ItemOne.should("have.text", "Sauce Labs Onesie");
+    LoginPage.ItemCosts.should("have.text", "$7.99");
+     })
+  it("Scenario 6", () => {
+    LoginPage.visit();
+    LoginPage.UsernameField.click();
+    LoginPage.UsernameField.type('standard_user');
+    LoginPage.PasswordField.click();
+    LoginPage.PasswordField.type('secret_sauce');
+    LoginPage.LoginButton.click();
+    LoginPage.SortButton.select("Name (Z to A)");
+    LoginPage.AnotherFirst.should("have.text", "Test.allTheThings() T-Shirt (Red)");
+  })
+  it("Scenario 7", () => {
+    LoginPage.visit();
+    LoginPage.UsernameField.click();
+    LoginPage.UsernameField.type('standard_user');
+    LoginPage.PasswordField.click();
+    LoginPage.PasswordField.type('secret_sauce');
+    LoginPage.LoginButton.click();
+    LoginPage.BoltTshirt.click();
+    LoginPage.AddToCard.click();
+    LoginPage.BadgeButton.should("contain", "1");
+    LoginPage.BackToProducts.click();
+    LoginPage.AnotherTshirt.click();
+    LoginPage.AddToCardTwo.click();
+    LoginPage.BadgeButton.should("contain", "2");
+  })
+  it("Scenario 8", () => {
+    LoginPage.visit();
+    LoginPage.UsernameField.click();
+    LoginPage.UsernameField.type('standard_user');
+    LoginPage.PasswordField.click();
+    LoginPage.PasswordField.type('secret_sauce');
+    LoginPage.LoginButton.click();
+    LoginPage.BoltTshirt.click();
+    LoginPage.AddToCard.click();
+    LoginPage.BackToProducts.click();
+    LoginPage.BadgeButton.should("contain", "1");
+    LoginPage.BurgerButton.click();
+    LoginPage.ResetButton.click();
+    LoginPage.BadgeButton.should('not.exist');
+    
+  })
+
+  it("Scenario 9", () => {
+    LoginPage.visit();
+    LoginPage.UsernameField.click();
+    LoginPage.UsernameField.type('standard_user');
+    LoginPage.PasswordField.click();
+    LoginPage.PasswordField.type('secret_sauce');
+    LoginPage.LoginButton.click();
+    LoginPage.BoltTshirt.click();
+    LoginPage.AddToCard.click();
+    LoginPage.BadgeButton.should("contain", "1");
+    LoginPage.RemoveButton.click();
+    LoginPage.BadgeButton.should('not.exist');
+
+  })
+  it("Scenario 10", () => {
+    LoginPage.visit();
+    LoginPage.UsernameField.click();
+    LoginPage.UsernameField.type('standard_user');
+    LoginPage.PasswordField.click();
+    LoginPage.PasswordField.type('secret_sauce');
+    LoginPage.LoginButton.click();
+    LoginPage.AnotherFirst.click();
+    LoginPage.AddToCardThree.click();
+    LoginPage.ShoppingCart.click();
+    LoginPage.Checkout.click();
+    LoginPage.FirstName.click();
+    LoginPage.FirstName.type('Marta');
+    LoginPage.LastName.click();
+    LoginPage.LastName.type('Liepa');
+    LoginPage.PostalCode.click();
+    LoginPage.PostalCode.type('1084');
+    LoginPage.Continue.click();
+    LoginPage.ItemName.should('contain', 'Test.allTheThings() T-Shirt (Red)')
+    LoginPage.Finish.click();
+    LoginPage.Header.should('contain', 'THANK YOU FOR YOUR ORDER');
+  })
+  
+  
+
+  
+
+
 });
--- cypress/pageObjects/Login.page.js
+++ cypress/pageObjects/Login.page.js
@@ -4,10 +4,100 @@ class LoginPage extends BasePage {
   static get url() {
     return "/";
   }
-
   static get xxx() {
     return cy.get("x[y=z]");
   }
-}
+  static get UsernameField() {
+    return cy.get("#user-name");
+  }
+  static get PasswordField() {
+    return cy.get("#password");
+  }
+  static get LoginButton() {
+    return cy.get("#login-button");
+  }
+  static get Notification() {
+    return cy.get("[data-test=error]");
+  }
+  static get Default() {
+    return cy.get(".inventory_item_description");
+  }
+  static get SortButton() {
+    return cy.get('[class="product_sort_container"]');
+  }
+  static get FirstItem() {
+    return cy.get('#item_5_title_link > .inventory_item_name');
+  }
+  static get ItemCosts() {
+    return cy.get(`:nth-child(1) > .inventory_item_description > .pricebar > .inventory_item_price`);
+  }
+  static get ItemOne() {
+    return cy.get(`#item_2_title_link > .inventory_item_name`);
+  }
+  static get Itemprice() {
+    return cy.get(`:nth-child(1) > .inventory_item_description > .pricebar > .inventory_item_price`);
+  }
+  static get AnotherFirst() {
+    return cy.get('#item_3_title_link > .inventory_item_name');
+  }
+  static get BoltTshirt() {
+    return cy.get(`#item_1_title_link > .inventory_item_name`);
+  }
+  static get AddToCard() {
+    return cy.get(`[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]`);
+  }
+  static  get BadgeButton() {
+    return cy.get(".shopping_cart_badge");
+  }
+  static get BackToProducts() {
+    return cy.get(`[data-test="back-to-products"]`);
+  }
+  static get AnotherTshirt() {
+    return cy.get("#item_0_title_link > .inventory_item_name");
+  }
+  static get AddToCardTwo() {
+    return cy.get(`[data-test="add-to-cart-sauce-labs-bike-light"]`);
+  }
+  static get BurgerButton() {
+    return cy.get("#react-burger-menu-btn");
+  }
+  static get ResetButton() {
+    return cy.get('#reset_sidebar_link');
+  }
+  static get RemoveButton() {
+    return cy.get(`[data-test="remove-sauce-labs-bolt-t-shirt"]`);
+  }
+  static get AddToCardThree() {
+    return cy.get(`[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]`);
+  }
+  static get ShoppingCart() {
+    return cy.get(".shopping_cart_link");
+  }
+  static get Checkout() {
+    return cy.get (`[data-test="checkout"]`);
+  }
+  static get FirstName() {
+    return cy.get(`[data-test="firstName"]`);
+  }
+  static get LastName() {
+    return cy.get(`[data-test="lastName"]`);
+  }
+  static get PostalCode() {
+    return cy.get(`[data-test="postalCode"]`);
+  }
+  static get Continue() {
+    return cy.get(`[data-test="continue"]`);
+  }
+  static get ItemName() {
+    return cy.get(".inventory_item_name");
+  }
+  static get Finish() {
+    return cy.get(`[data-test="finish"]`);
+  }
+  static get Header() {
+    return cy.get('.complete-header');
+  }
 
+  
+}
 export default LoginPage;
